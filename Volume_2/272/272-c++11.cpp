5351c2ae6fdeb2aa58af48d9954359422cb724f2
LI, HONG-YU <zhenlinospirit@gmail.com>
2017-02-06 21:54:52 +0800

    [272] [Easy] Directly output the left-single-quote or right-single-quote.
    Summary: At the first glance, I use the unnecessary function like replace of std::string.
    It complicates the implementation.

---
+++ Volume_2/272/272-c++11.cpp
@@ -0,0 +1,26 @@
+// -lm -lcrypt -O2 -std=c++11 -pipe -DONLINE_JUDGE
+#include <iostream>
+#include <string> // std::getline
+
+using namespace std;
+
+// R"d(")d" ... C++11 raw string literal
+
+int main()
+{
+        string line;
+        auto flip = 0;
+        string quotes[2] = { string("``"), string("''") };
+        while(std::getline(std::cin, line)) {
+                for(const auto& i : line) {
+                        if(i == '\"') {
+                                cout << quotes[flip];
+                                flip = 1 - flip;
+                        } else {
+                                cout << i;
+                        }
+                }
+                cout << endl;
+        }
+        return 0;
+}
