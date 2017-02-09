ee3aec3964d6d1589f558ac0d6b45e8d584dbf98
LI, HONG-YU <zhenlinospirit@gmail.com>
2017-02-09 22:55:28 +0800

    [272] Reformat the code.

--- Volume_2/272/272-c++11.cpp
+++ Volume_2/272/272-c++11.cpp
@@ -6,21 +6,20 @@ using namespace std;
 
 // R"d(")d" ... C++11 raw string literal
 
-int main()
-{
-        string line;
-        auto flip = 0;
-        string quotes[2] = { string("``"), string("''") };
-        while(std::getline(std::cin, line)) {
-                for(const auto& i : line) {
-                        if(i == '\"') {
-                                cout << quotes[flip];
-                                flip = 1 - flip;
-                        } else {
-                                cout << i;
-                        }
-                }
-                cout << endl;
+int main() {
+    string line;
+    auto flip = 0;
+    string quotes[2] = { string("``"), string("''") };
+    while(std::getline(std::cin, line)) {
+        for(const auto& i : line) {
+            if(i == '\"') {
+                cout << quotes[flip];
+                flip = 1 - flip;
+            } else {
+                cout << i;
+            }
         }
-        return 0;
+        cout << endl;
+    }
+    return 0;
 }
